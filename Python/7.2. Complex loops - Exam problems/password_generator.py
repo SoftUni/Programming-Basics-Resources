3409d0f533b424f05b6a36b0a22a657f97088bbb
Antoveravip <petar@petarivanov.com>
2018-07-23 22:14:00 +0300

    Chapter 7.2 Change result display

--- Python/7.2. Complex loops - Exam problems/password_generator.py
+++ Python/7.2. Complex loops - Exam problems/password_generator.py
@@ -7,5 +7,8 @@ for s1 in range(1, n + 1):
         for s3 in range(ord('a'), l + ord('a')):
             for s4 in range(ord('a'), l + ord('a')):
                 for s5 in range(max(s1,s2) + 1, n + 1):
-                    result += '' + str(s1) + str(s2) + chr(s3) + chr(s4) + str(s5) + ' '
-print(result)
+                    result += ('' + str(s1) + str(s2)
+                               + chr(s3) + chr(s4)
+                               + str(s5) + ' ')
+print(result.strip())
+
